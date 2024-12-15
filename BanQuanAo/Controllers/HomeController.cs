e9ef3b06bcadb656ff68092a7195736ca303abb3
congkcks <congvanvu00@gmail.com>
2024-12-16 01:29:06 +0700

    Add project files.

---
+++ BanQuanAo/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using BanQuanAo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PagedList;
+using System.Diagnostics;
+
+namespace BanQuanAo.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly QlbanHangQuanAoContext _context;
+
+		public HomeController(ILogger<HomeController> logger, QlbanHangQuanAoContext context)
+		{
+			_logger = logger;
+			_context = context;
+		}
+
+		public IActionResult Index(int? page)
+		{
+			var PhanLoaiChinh = _context.PhanLoais.ToList();
+			var danhsachphanloaiphu= _context.PhanLoaiPhus.ToList();
+			var danhsachmenu=new List<PhanTong>();
+			foreach(var item in PhanLoaiChinh)
+			{
+				var phantong = new PhanTong();
+				phantong.PhanLoaiChinh = item;
+				phantong.DanhSachPhanLoaiPhu = danhsachphanloaiphu.Where(p => p.MaPhanLoai == item.MaPhanLoai).ToList();
+				danhsachmenu.Add(phantong);
+			}
+			ViewBag.DanhSachMenu = danhsachmenu;
+			int pageSize = 6;
+			int pageNumber = (page ?? 1);
+			var sanpham = _context.SanPhams.ToList();
+			ViewBag.HienThi = sanpham;
+			return View();
+		}
+		[Route("/{id}")]
+		public IActionResult ChiTietSanPham(string id)
+		{
+			var PhanLoaiChinh = _context.PhanLoais.ToList();
+			var danhsachphanloaiphu = _context.PhanLoaiPhus.ToList();
+			var danhsachmenu = new List<PhanTong>();
+			foreach (var item in PhanLoaiChinh)
+			{
+				var phantong = new PhanTong();
+				phantong.PhanLoaiChinh = item;
+				phantong.DanhSachPhanLoaiPhu = danhsachphanloaiphu.Where(p => p.MaPhanLoai == item.MaPhanLoai).ToList();
+				danhsachmenu.Add(phantong);
+			}
+			ViewBag.DanhSachMenu = danhsachmenu;
+			var sanpham = _context.SanPhams.Where(p => p.MaSanPham ==id).FirstOrDefault();
+			return View(sanpham);
+		}
+		public IActionResult PhanLoaiAPI(string danhsachchinh)
+		{
+			var maphanloai = (from p in _context.PhanLoais
+							  where p.PhanLoaiChinh == danhsachchinh
+							  select p.MaPhanLoai).FirstOrDefault();
+			var danhsach= _context.SanPhams.Where(p => p.MaPhanLoai == maphanloai).ToList();
+		    return PartialView("SanPham",danhsach);
+		}
+		public IActionResult PhanLoaiPhuAPI(string danhsachphu)
+		{
+			var maphanloai = (from p in _context.PhanLoaiPhus
+							  where p.TenPhanLoaiPhu == danhsachphu
+							  select p.MaPhanLoaiPhu).FirstOrDefault();
+			var danhsach = _context.SanPhams.Where(p => p.MaPhanLoaiPhu == maphanloai).ToList();
+			return PartialView("SanPham", danhsach);
+		}
+		public IActionResult GetPagedProducts(int ? page,int? pageSize)
+		{
+			int pageNumber = (page ?? 1);
+			int size = (pageSize ?? 6);
+
+			var sanpham = _context.SanPhams.OrderBy(p => p.TenSanPham).ToPagedList(pageNumber, size);
+			return PartialView("SanPham", sanpham);
+			
+		}
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ BanQuanAo/Models/PhanTong.cs
@@ -0,0 +1,8 @@
+﻿namespace BanQuanAo.Models
+{
+	public class PhanTong
+	{
+		public PhanLoai PhanLoaiChinh { get; set; }
+		public List<PhanLoaiPhu> DanhSachPhanLoaiPhu { get; set; }
+	}
+}
